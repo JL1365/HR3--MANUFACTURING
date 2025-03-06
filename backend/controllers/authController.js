58a81f20e49f3939bb9c3f00350a42ddfec0d423
noah1365 <borlagdatanjohnlloyd1@gmail.com>
2025-03-06 14:18:01 +0800

    Implement login functionality; add token generation and cookie setting

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import bcrypt from 'bcryptjs';
 
 import { generateServiceToken } from "../middlewares/gatewayTokenGenerator.js"
+import { generateTokenAndSetCookie } from '../utils/generateTokenAndSetCookie.js';
 
 export const getAccountsFromAdmin = async (req,res) => {
     try {
@@ -16,4 +17,35 @@ export const getAccountsFromAdmin = async (req,res) => {
         console.error(`Error in getting accounts:${error.message}`);
         res.status(500).json({ message: "Internal Server error" });
     }
+}
+
+export const login = async (req,res) => {
+    try {
+        const { email, password } = req.body;
+    
+        const serviceToken = generateServiceToken();
+    
+        const response = await axios.get(`${process.env.API_GATEWAY_URL}/admin/get-accounts`, {
+            headers: { Authorization: `Bearer ${serviceToken}` }
+        });
+    
+        const users = response.data;
+        const user = users.find((u) => u.email === email);
+    
+        if (!user) {
+          return res.status(400).json({ message: "Invalid email or password" });
+        }
+    
+        const isPasswordMatch = await bcrypt.compare(password, user.password);
+        if (!isPasswordMatch) {
+          return res.status(400).json({ message: "Invalid email or password" });
+        }
+
+        const token = generateTokenAndSetCookie(res, user);
+        
+        return res.status(200).json({ token, user });
+      } catch (err) {
+        console.error("Error during login:", err.message);
+        return res.status(500).json({ message: "Server error" });
+      }
 }
--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,10 +1,11 @@
 import express from 'express';
 
-import { getAccountsFromAdmin } from '../controllers/authController.js';
+import { getAccountsFromAdmin, login } from '../controllers/authController.js';
 import { serviceVerifyToken } from '../middlewares/verifyToken.js';
 
 const authRoute = express.Router();
 
 authRoute.get("/get-accounts-from-admin",serviceVerifyToken,getAccountsFromAdmin);
+authRoute.post("/login",login);
 
 export default authRoute;
